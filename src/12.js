b1ac32c421d37f9d2f168aacbf5d17f8ba34ca8a
mavesommers <mavesommers@gmail.com>
2025-03-18 09:32:15 +0000

    Add 12.js

---
+++ src/12.js
@@ -0,0 +1,5 @@
+// Generate a random number between 1 and 100
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+// Print the result
+console.log(`The generated number is: ${randomNumber}`);
