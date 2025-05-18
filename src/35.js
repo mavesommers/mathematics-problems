b1ab7b95b5e6859159ed3d886c71af1425c40098
mavesommers <mavesommers@gmail.com>
2025-05-18 10:46:12 +0000

    Add 35.js

---
+++ src/35.js
@@ -0,0 +1,7 @@
+// Example JavaScript code to illustrate a basic calculation and display
+
+let number1 = 5;
+let number2 = 3;
+
+console.log(`The sum of ${number1} and ${number2} is:`, (number1 + number2));
+
